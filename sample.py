7cf290968eab4a25864b75203473fc3bcdf772f4
wisteria30 <wis30@wisteria30.local>
2018-12-06 04:00:38 +0900

    できたか？

---
+++ sample.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+import datetime
+
+# options = Options()
+# options.add_argument("--headless")
+# options.add_argument("--disable-gpu")
+# driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome()
+# ドライバが設定されるまでの待ち時間を設定する。
+driver.implicitly_wait(10)
+
+
+for i in range(29):
+    # トップ画面を開く
+    print("open category {}/28".format(i))
+    driver.get("https://valu.is/users/categories/")
+    # ローディング待ち
+    time.sleep(3)
+    # list-count(145) → int: 145
+    member_num = driver.find_element_by_xpath(
+        '//*[@id="wrap"]/article/div/div[3]/a[{}]/div/span'.format(i + 1)
+    ).text
+    member_num = member_num.replace("(", "").replace(")", "")
+    member_num = int(member_num)
+    print(member_num)
+    # 上のカテゴリーから順に入っていく
+    driver.find_element_by_xpath(
+        '//*[@id="wrap"]/article/div/div[3]/a[{}]'.format(i + 1)
+    ).click()
+    # スクロール下までしてリストを全て表示する
+    # actions = ActionChains(driver)
+    # scroll_class = driver.find_element_by_class_name("va-footer")
+    # actions.move_to_element(scroll_class)
+    # endTime = datetime.datetime.now() + datetime.timedelta(minutes=3)
+    # while True:
+    #     actions.perform()
+    #     if datetime.datetime.now() >= endTime:
+    #         break
+    # [actions.perform() for _ in range(5000)]
+    # お金を全てリストで取得
+    moneys = driver.find_elements_by_class_name("va-list-item-unit__number")
+    print("--------------------{}------------------------".format(len(moneys)))
+    driver.find_element_by_xpath(
+        '//*[@id="wrap"]/article/div/section/div[2]/div/a[1]'
+    ).click()
+    driver.find_element_by_xpath(
+        '//*[@id="wrap"]/div[8]/div[2]/div[2]/nav/div/ul/li[2]/a'
+    ).click()
+    money = driver.find_element_by_xpath('//*[@id="wrap"]/div[2]/div[2]/div[2]/div[2]/section/div/table/tbody/tr[3]/td/span').text
+    print(money)
+    driver.back()
+    driver.back()
+    # print([money.text for money in moneys])
+    driver.back()
+print("finish!!!")
+# ドライバーを終了
+driver.close()
+
+
+# def member_crawl(n):
+#     num = 1
+#     jscroll_added = 0
+#     while n != num:
+#         print("now div{}/div{} // {}".format(jscroll_added, num, n))
+#         # 0なら普通のX-PATH それ以外は addedのX-PATHをクリック
+#         if not jscroll_added:
+#             driver.find_element_by_xpath(
+#                 '//*[@id="wrap"]/article/div/section/div[2]/div/a[{}]'.format(num)
+#             ).click()
+#         else:
+#             driver.find_element_by_xpath(
+#                 '//*[@id="wrap"]/article/div/section/div[2]/div/div[{}]/div/a[{}]'.format(
+#                     jscroll_added, num
+#                 )
+#             ).click()
+#         # スクショ撮って保存
+#         take_screenshot()
+#         # 20越えたらaddedのX-PATHの方へ
+#         if num is 20:
+#             num = 1
+#             jscroll_added += 1
+#         else:
+#             num += 1
+#     # リンク一つ戻る
+#     driver.back()
--- valu_scraping.py
+++ valu_scraping.py
@@ -3,73 +3,89 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 
-options = Options()
+# options = Options()
 # headlessで使用する場合は以下の2行を利用する。
-options.add_argument("--headless")
-options.add_argument("--disable-gpu")
-driver = webdriver.Chrome(options=options)
+# options.add_argument("--headless")
+# options.add_argument("--disable-gpu")
+# driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome()
 # ドライバが設定されるまでの待ち時間を設定する。
 driver.implicitly_wait(10)
-# valuのカテゴリページ
-path = "https://valu.is/users/categories/"
 
 
-def category_crawl():
+def category_crawl(path):
     for i in range(29):
         # トップ画面を開く
-        driver.get(path + str(i))
+        print("open category {}/28".format(i))
+        driver.get(path)
         # ローディング待ち
         time.sleep(3)
         # list-count(145) → int: 145
-        member_num = int(
-            driver.find_element_by_xpath(
-                '//*[@id="wrap"]/article/div/div[3]/a[{}]/div/span'.format(i + 1)
-            )
-            .text.split(")", "")
-            .split("(", "")
-        )
+        member_num = driver.find_element_by_xpath(
+            '//*[@id="wrap"]/article/div/div[3]/a[{}]/div/span'.format(i + 1)
+        ).text
+        member_num = member_num.replace("(", "").replace(")", "")
+        member_num = int(member_num)
         # 上のカテゴリーから順に入っていく
         driver.find_element_by_xpath(
             '//*[@id="wrap"]/article/div/div[3]/a[{}]'.format(i + 1)
         ).click()
-        scraping(member_num)
+        member_crawl(member_num)
+    print("finish!!!")
+    # ドライバーを終了
+    driver.close()
+    # driver.quit()
 
 
-def scraping(n):
-    jscroll_added = n // 20
+def member_crawl(n):
     num = 1
+    jscroll_added = 0
     while n != num:
-        while num <= 20:
+        print("now div{}/div{} // {}".format(jscroll_added, num, n))
+        # 0なら普通のX-PATH それ以外は addedのX-PATHをクリック
+        if jscroll_added is 0:
             driver.find_element_by_xpath(
                 '//*[@id="wrap"]/article/div/section/div[2]/div/a[{}]'.format(num)
             ).click()
-            take_screenshot()
-            driver.back()
-            num += 1
-        for k in range(jscroll_added):
+        else:
             driver.find_element_by_xpath(
                 '//*[@id="wrap"]/article/div/section/div[2]/div/div[{}]/div/a[{}]'.format(
-                    k + 1, num
+                    jscroll_added, num
                 )
             ).click()
-            take_screenshot()
-            driver.back()
-            num = 1 if num is 20 else num + 1
+        # スクショ撮って保存
+        take_screenshot()
+        # 20越えたらaddedのX-PATHの方へ
+        if num is 20:
+            num = 1
+            jscroll_added += 1
+        else:
+            num += 1
+    # リンク一つ戻る
+    driver.back()
 
 
 def take_screenshot():
+    # スクショ
     png = driver.find_element_by_xpath(
         '//*[@id="wrap"]/div[8]/div[2]/div[2]/div[1]/section[1]/div/div/span/span'
     ).screenshot_as_png
+    # お金の金額を取る
+    driver.find_element_by_xpath(
+        '//*[@id="wrap"]/div[8]/div[2]/div[2]/nav/div/ul/li[2]/a'
+    ).click()
     money = driver.find_element_by_xpath(
-        '//*[@id="wrap"]/div[8]/div[2]/div[2]/div[1]/section[2]/div/table/tbody[2]/tr/td/span'
+        '//*[@id="wrap"]/div[2]/div[2]/div[2]/div[2]/section/div/table/tbody/tr[3]/td/span'
     ).text
-    print(money)
+    money = money.replace("BTC", "")
+    driver.back()
     # 画像を保存
-    with open("./img/" + money, "wb") as f:
+    with open("./img/" + money + ".png", "wb") as f:
         f.write(png)
+    print("export picture ./img/{}.png!!!!!".format(money))
+    # リンク一つ戻る
+    driver.back()
 
 
-# ドライバーを終了
-driver.close()
-# driver.quit()
+# valuのカテゴリページ
+category_crawl("https://valu.is/users/categories/")
