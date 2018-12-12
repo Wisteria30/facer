6127aeb1404a8b7329d3ad9b42786cd47a182aa1
wisteria30 <wis30@wisteria30.local>
2018-12-12 19:05:32 +0900

    スクレイプスクリプト完成

--- valu_scraping.py
+++ valu_scraping.py
@@ -1,24 +1,21 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
-import time
 import os
 import random
+import shutil
+import time
 
-# headlessで使用する場合は以下の2行を利用する。
-# options = Options()
-# options.add_argument("--headless")
-# options.add_argument("--disable-gpu")
-# driver = webdriver.Chrome(options=options)
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+
 driver = webdriver.Chrome()
 # ドライバが設定されるまでの待ち時間を設定する。
 driver.implicitly_wait(10)
 
 
 def category_crawl(path):
-    for i in range(29):
+    for i in range(28, 29):
         # トップ画面を開く
         print("open category {}/28".format(i))
         driver.get(path)
@@ -52,43 +49,52 @@ def category_crawl(path):
 
 
 def member_crawl(n):
+    # カウント用
+    count = 0
+    # X-Pathのパス用
     num = 1
     jscroll_added = 0
-    while n != num:
+    while n >= count:
         # 0なら普通のX-PATH それ以外は addedのX-PATHをクリック
-        if jscroll_added is 0:
-            driver.find_element_by_xpath(
-                '//*[@id="wrap"]/article/div/section/div[2]/div/a[{}]'.format(num)
-            ).send_keys(Keys.COMMAND, Keys.ENTER)
-        else:
-            driver.find_element_by_xpath(
-                '//*[@id="wrap"]/article/div/section/div[2]/div/div[{}]/div/a[{}]'.format(
-                    jscroll_added, num
-                )
-            ).send_keys(Keys.COMMAND, Keys.ENTER)
-        # タブの変更
-        driver.switch_to.window(driver.window_handles[1])
-        # スクショ撮って保存
-        take_screenshot()
-        # タブの消滅
-        driver.close()
-        # 元のタブに戻る
-        driver.switch_to.window(driver.window_handles[0])
+        try:
+            if jscroll_added is 0:
+                driver.find_element_by_xpath(
+                    '//*[@id="wrap"]/article/div/section/div[2]/div/a[{}]'.format(num)
+                ).send_keys(Keys.COMMAND, Keys.ENTER)
+            else:
+                driver.find_element_by_xpath(
+                    '//*[@id="wrap"]/article/div/section/div[2]/div/div[{}]/div/a[{}]'.format(
+                        jscroll_added, num
+                    )
+                ).send_keys(Keys.COMMAND, Keys.ENTER)
+            # print("num={}, jscroll_added={}".format(num, jscroll_added))
+            # タブの変更
+            driver.switch_to.window(driver.window_handles[1])
+            # スクショ撮って保存
+            take_screenshot()
+            # タブの消滅
+            driver.close()
+            # 元のタブに戻る
+            driver.switch_to.window(driver.window_handles[0])
+        except:
+            num += 1
         # 20越えたらaddedのX-PATHの方へ
-        if num is 20:
+        if num >= 20:
             num = 1
             jscroll_added += 1
         else:
             num += 1
+        count += 1
     # リンク一つ戻る
     driver.back()
 
 
 def take_screenshot():
     # スクリーンショット
-    png = driver.find_element_by_class_name(
-        "va-img-thumb-cropped__inner"
-    ).screenshot_as_png
+    icon = driver.find_element_by_class_name("va-img-thumb-cropped__inner")
+    jpg = icon.value_of_css_property("background-image")
+    jpg = jpg.replace('url("', "").replace('")', "")
+    res = requests.get(jpg, stream=True)
     # お金の金額を取る
     driver.find_element_by_link_text("データ").click()
     try:
@@ -102,13 +108,14 @@ def take_screenshot():
     money = money.replace("BTC", "")
     driver.back()
     # 同一ファイル名が存在するか確認
-    filename = "./img/" + money + ".png"
+    filename = "./image/original/" + money + ".jpg"
     if os.path.exists(filename):
-        filename = "./img/" + money + "__" + str(random.random()) + ".png"
+        filename = "./image/original/" + money + "__" + str(random.random()) + "__.jpg"
     # 画像を保存
     with open(filename, "wb") as f:
-        f.write(png)
-    print("export picture ./img/{}.png!!!!!".format(filename))
+        shutil.copyfileobj(res.raw, f)
+        # f.write(jpg)
+    # print("export picture {}!!!!!".format(filename))
 
 
 if __name__ == "__main__":
